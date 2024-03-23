0181f3be76eb1b1e98df8f49f3503980d3c862ac
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2024-03-22 20:10:03 -0400

    Initial Commit

---
+++ DubGrub/DubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubGrubApp.swift
+//  DubGrub
+//
+//  Created by Deonte Kilgore on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
